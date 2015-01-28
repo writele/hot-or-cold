5411544466b42a1c6626a1203d8a532a22a89f93
Ele Palmer <ecpalmer21@gmail.com>
2015-01-28 14:17:07 -0500

    Quiz Fully Functional

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,57 +11,68 @@ $( document ).ready(function() {
   	});
 
 // VARIABLES 
-var newNumber = Math.floor(Math.random() * 10) + 1;
+var newNumber = Math.floor(Math.random() * (100 - 1)) + 1;
 var guessCount = 0;
-var userGuess = $("#userGuess").val();
-
+    
+// NEW GAME
 var newGame = function (event) {
     //Generate and store random variable between 1 and 100
+    guessCount = 0;
+    $("#count").html(guessCount);
+    $("#feedback").html("<h2>Make your Guess!</h2>");
+    $("#guessList").empty();
+    $("#userGuess").val("");
     event.preventDefault();
-    return newNumber;
-    //Clear all previously stored values (See below)
+    newNumber = Math.floor(Math.random() * (100 - 1)) + 1;
 };
 
-var addGuess = function () {
+//GUESS A NUMBER 
+var makeGuess = function() {
+    //USER GUESS
+    var userGuess = $("#userGuess").val();
+    //ADD GUESS
+    var addGuess = function() {
     //increase guessCount by 1
-    $("#count").replaceWith(guessCount + 1);
+    guessCount = guessCount + 1;
+    $("#count").html(guessCount);
     //append guess to ul#guessList 
     $("#guessList").append("<li>" + userGuess + "</li>");
     };
-    
-//START NEW GAME 
-$(".new").on("click", newGame());
-
-    
-//GUESS A NUMBER
-$("#guessButton").on("click", function (event) {
-    event.preventDefault();
-    if (userGuess === newNumber) {
-        $("#feedback").find("h2").replaceWith("Way to go! That is correct! Click on '+NEW GAME' to start a new game."); 
+    //CHECK IF HOT OR COLD
+    if (userGuess == newNumber) {
+        $("#feedback").html("<h2>Way to go! That is correct! Click on '+NEW GAME' to start a new game.</h2>"); 
     }
-    else if (userGuess === NaN || userGuess > 100 || userGuess < 1) 
-    {
-    $("#feedback").find("h2").replaceWith("Brrr! Freezing!");
+    else if (userGuess == NaN || userGuess > 100 || userGuess < 1) {
+        $("#feedback").html("<h2>That is not a number!</h2>");
         addGuess();
     }
-    else if (userGuess >= newNumber + 50 || userGuess <= newNumber - 50)           $("#feedback").find("h2").replaceWith("Brrr! Freezing!");
+    else if (userGuess >= newNumber + 50 || userGuess <= newNumber - 50)   {        
+        $("#feedback").html("<h2>Brrr! Freezing!</h2>");
         addGuess();
     }
-    else if (userGuess >= newNumber + 30 || userGuess <= newNumber - 30) {      $("#feedback").find("h2").replaceWith("Cold.");
-    addGuess();
+    else if (userGuess >= newNumber + 30 || userGuess <= newNumber - 30) {      
+        $("#feedback").html("<h2>Cold.</h2>");
+        addGuess();
     }
-    else if (userGuess >= newNumber + 20 || userGuess <= newNumber - 20) {     $("#feedback").find("h2").replaceWith("Warm.");
-    addGuess();
-    }
-    else if (userGuess >= newNumber + 15 || userGuess <= newNumber - 15) {
-$("#feedback").find("h2").replaceWith("Hot!");
-    addGuess();  
+    else if (userGuess >= newNumber + 20 || userGuess <= newNumber - 20) {     
+        $("#feedback").html("<h2>Warm.</h2>");
+        addGuess();
     }
     else if (userGuess >= newNumber + 10 || userGuess <= newNumber - 10) {
-$("#feedback").find("h2").replaceWith("On Fire!!");
-    addGuess(); 
+        $("#feedback").html("<h2>Hot!</h2>");
+        addGuess();  
     }
-);
+    else if (userGuess >= newNumber + 1 || userGuess <= newNumber - 1) {
+        $("#feedback").html("<h2>On Fire!!</h2>");
+        addGuess(); 
+    } 
+};
+                  
+//MAKE GUESS                
+$("#guessButton").on("click", makeGuess);
+//START NEW GAME 
+$(".new").on("click", newGame); 
+
 
 });
 
